ad70985aa22678f467f4add882c1a2d84d756e91
MohammedSB <MohammedSalimAB@outlook.com>
2024-02-08 23:45:45 +0300

    bug fixes and adding bonus results

--- HemaDataset.py
+++ HemaDataset.py
@@ -6,12 +6,13 @@ import torch
 from monai.networks import one_hot
 
 class HemaDataset(torch.utils.data.Dataset):
-    def __init__(self, root, image_transform=None, target_transform=None, seg_entire_cell=True, split="train"):
+    def __init__(self, root, image_transform=None, target_transform=None, seg_entire_cell=True, split="train", return_meta=False):
         self.root = root
         self.split = split
         self.image_transform = image_transform
         self.target_transform = target_transform
         self.seg_entire_cell = seg_entire_cell
+        self.return_meta = return_meta
         self.define_split()
 
     def define_split(self):
@@ -22,10 +23,10 @@ class HemaDataset(torch.utils.data.Dataset):
         elif self.split == "val":
             self.images_root = self.root + os.sep + "imagesVl"
             self.labels_root = self.root + os.sep + "labelsVl"
-        elif self.split == "ex-test":
+        elif self.split == "test-ex":
             self.images_root = self.root + os.sep + "imagesTs-External"
             self.labels_root = None
-        elif self.split == "in-test" or self.split == "test":
+        elif self.split == "test-in" or self.split == "test":
             self.images_root = self.root + os.sep + "imagesTs-Internal"
             self.labels_root = None
         else:
@@ -40,11 +41,20 @@ class HemaDataset(torch.utils.data.Dataset):
     
     def __getitem__(self, indx):
         image = self.images[indx]
-        label = self.labels[indx]
-
         image = Image.open(image)
-        label = Image.open(label)
-
+                
+        if self.labels_root != None:
+            label = self.labels[indx]
+            label = Image.open(label)
+        else:
+            label = None
+        
+        # Define meta variables
+        meta = {}
+        width, height = image.size
+        meta["path"] = self.images[indx]
+        meta["size"] = width
+            
         seed = np.random.randint(2147483647) # make a seed with numpy generator
         if self.image_transform != None:
             np.random.seed(seed), torch.manual_seed(seed) 
@@ -54,12 +64,14 @@ class HemaDataset(torch.utils.data.Dataset):
             np.random.seed(seed), torch.manual_seed(seed) 
             label = self.target_transform(label)
         
-        # one hot encode the label
-        label = one_hot(label.unsqueeze(0), num_classes=3).squeeze(0)
-        if self.seg_entire_cell: # if we want to segment the entire cell as a class, we assign labels 1+2 as a new label 1.
-            cyto = (label[1:2, :, :] == 1)
-            nucleus = (label[2:3, :, :] == 1)
-            entire_cell = cyto | nucleus
-            label[1:2, :, :] = torch.where(entire_cell, torch.tensor(1.0, device=label.device), label[1:2, :, :])
-
+        if self.labels_root != None:
+            # one hot encode the label
+            label = one_hot(label.unsqueeze(0), num_classes=3).squeeze(0)
+            if self.seg_entire_cell: # if we want to segment the entire cell as a class, we assign labels 1+2 as a new label 1.
+                cyto = (label[1:2, :, :] == 1)
+                nucleus = (label[2:3, :, :] == 1)
+                entire_cell = cyto | nucleus
+                label[1:2, :, :] = torch.where(entire_cell, torch.tensor(1.0, device=label.device), label[1:2, :, :])
+        if self.return_meta:
+            return image, label, meta
         return image, label
